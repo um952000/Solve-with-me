68bd9c5b47ee2a4dbe264885cf3add9c92a08a40
Utkarsh Mishra <um952000@gmail.com>
2023-07-25 22:24:47 +0530

    5th

--- client/src/styles.js
+++ client/src/styles.js
@@ -3,7 +3,7 @@ import { deepPurple } from '@material-ui/core/colors';
 
 export default makeStyles((theme) => ({
   appBar: {
-    borderRadius: 10,
+    borderRadius: 15,
     margin: '30px 0',
     display: 'flex',
     flexDirection: 'row',
