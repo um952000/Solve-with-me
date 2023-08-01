348c7e581110e1aba1a8a145e2826d628df60b24
Utkarsh Mishra <um952000@gmail.com>
2023-08-01 19:35:34 +0530

    asds

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-             <h1>App</h1>
+             <h1>App1</h1>
+             
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,30 @@
-import express from 'express';
+import express, { application } from 'express';
 // body-parser is a middleware that is used to parse the incoming request bodies in a more convenient way. 
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 // CORS (Cross-Origin Resource Sharing) is a security feature implemented by web browsers to restrict webpages from making requests to a different domain than the one that served the webpage. This is a mechanism designed to enhance the security of web applications and prevent unauthorized access to resources.
 import cors from 'cors';
+
+//importing postRoutes from routes folder
+import postRoutes from './routes/posts.js';
+
+
+const app = express();
+
+//using express middleware to connect this route to our application
+app.use('./posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended:true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended:true}));
+app.use(cors());
+
+//creating a database url
+const CONNECTION_URL = 'mongodb+srv://Utkarsh:5ogNGOBB6h0qaXIf@cluster0.trtblhz.mongodb.net/?retryWrites=true&w=majority';
+//creating a port number
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+//mongoose.set('useFindAndModify', false);
