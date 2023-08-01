15bb5de2b673bf23c673a59ad1f2d8cc057a7c76
Utkarsh Mishra <um952000@gmail.com>
2023-08-01 07:56:23 +0530

    jeeelo

--- client/src/api/index.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const url = 'http://localhost:5000/posts/';
-
-//function to fetching post from the url that are in the database......................
-export const fetchPosts = () => axios.get(url);
--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-// this is the file for the combination of the reducers
-
-import { combineReducers } from "redux";
-
-import posts from "./posts";
-
-export default combineReducers({posts});
